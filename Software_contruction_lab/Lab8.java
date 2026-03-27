d66f2019bebe6a8472387868f99f8368f90a6b46
Student-MuhammadAbubakar <muhammadabubakar76969@gmail.com>
2026-03-27 00:35:37 -0700

    Lab 8 SDC

---
+++ Software_contruction_lab/Lab8.java
@@ -0,0 +1,33 @@
+public class Lab8 {
+    public static void main(String[] args) {
+        // int Age=25;
+        // int votingAge=10;
+        // if (Age>=votingAge){
+        //     System.out.println("Old enough to vote");
+        // }
+        // else {
+        //     System.out.println("Not enough to vote");
+        // }
+        // int time =20;
+        // String message=(time<12) ? "Good morning."
+        //             :  (time<18) ? "Good afternoon"
+        //             :  "Good Evening";
+        // System.out.println(message);
+    //     int sum=0;
+    //     for(int i=1;i<=20;i++){
+    //         sum+=i;
+    //     }
+    //     System.out.printf("Sum is of 20 natural numbers are  %s",sum);
+    // int i=5;
+    // while (i>=0) {
+    //     System.out.println(i);
+    //     i=i-2;
+    // }    
+    for(int i=0;i<10;i++){
+        if(i==4){
+           break;
+        }
+        System.out.println(i);
+    }
+}
+}
